139609c9e73b18357fbdc982eeea3474edaf1d41
Shivam3220 <92621551+Shivam3220@users.noreply.github.com>
2023-10-14 11:57:48 +0530

    Update UserRoute.js

--- noteapp/server/Routes/UserRoute.js
+++ noteapp/server/Routes/UserRoute.js
@@ -3,7 +3,7 @@ const UserSchema = require("../db/Schemas/UserSchema")
 const router = express.Router()
 const bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
-
+const SECRET ="thiisss"
 router.post("/add-user",async (req,res)=>{
     const{name, email, password, cpassword} = req.body;
     if(password===cpassword){
@@ -12,7 +12,7 @@ router.post("/add-user",async (req,res)=>{
             const user = await UserSchema.findOne({"email":email})
             if(!user){
                 const userAdded = await UserSchema.create({name:name , email:email , password:hashedPassword})
-                var token = jwt.sign({"id":userAdded.id,"name":userAdded.name}, process.env.SECRET);
+                var token = jwt.sign({"id":userAdded.id,"name":userAdded.name}, SECRET);
                 res.status(200).send({"message":"user added successfuly","code":200 , user:token})
             }else{
 
@@ -29,7 +29,7 @@ router.post("/add-user",async (req,res)=>{
 
 router.post("/check-token",async (req,res)=>{
         try {
-            var decoded = jwt.verify(req.body.token,  process.env.SECRET);
+            var decoded = jwt.verify(req.body.token,  .SECRET);
             res.status(200).send({"message":"token verified",user:decoded,"code":200})
         } catch (error) {
             res.status(500).send({"message":["token expire"],"code":400})
@@ -43,7 +43,7 @@ router.post("/log-in",async (req,res)=>{
            const user = await UserSchema.findOne({"email":email})
            const checkpass = await bcrypt.compare(password, user.password)
            if(checkpass){
-            var token = jwt.sign({"id":user.id , "name":user.name}, process.env.SECRET);
+            var token = jwt.sign({"id":user.id , "name":user.name},SECRET);
             res.status(200).send({"message":"you are logged in","token": token, "data":{"id":user.id , "name":user.name}, "code":200})
            }else{
             res.status(200).send({"message":"please enter valid credentials" , "code":400})
