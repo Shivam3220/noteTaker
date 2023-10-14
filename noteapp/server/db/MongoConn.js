d45dde9cf7fc2f79164bee2aceb5994dcf7a3eb7
Shivam3220 <92621551+Shivam3220@users.noreply.github.com>
2023-10-14 11:55:59 +0530

    Update MongoConn.js

--- noteapp/server/db/MongoConn.js
+++ noteapp/server/db/MongoConn.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 
 connectToMongo=()=>{
-  mongoose.connect(process.env.MONGO_URL,{
+  mongoose.connect("mongodb+srv://sunifi12:Y8B6NBTzUr71DXcJ@noteapp.wkk6pk8.mongodb.net/note-app",{
     useUnifiedTopology: true,
       useNewUrlParser: true,
       autoIndex: true
